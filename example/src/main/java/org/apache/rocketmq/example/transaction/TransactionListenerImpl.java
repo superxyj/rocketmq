ad19428ee8d48ab319faf6b13dee4d9adcf069cd
xieyajie <460121161@qq.com>
2021-03-20 12:59:03 +0800

    update事务消息

--- example/src/main/java/org/apache/rocketmq/example/transaction/TransactionListenerImpl.java
+++ example/src/main/java/org/apache/rocketmq/example/transaction/TransactionListenerImpl.java
@@ -25,33 +25,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TransactionListenerImpl implements TransactionListener {
-    private AtomicInteger transactionIndex = new AtomicInteger(0);
-
-    private ConcurrentHashMap<String, Integer> localTrans = new ConcurrentHashMap<>();
 
     @Override
     public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
-        int value = transactionIndex.getAndIncrement();
-        int status = value % 3;
-        localTrans.put(msg.getTransactionId(), status);
+        System.out.println("执行本地事务");
         return LocalTransactionState.UNKNOW;
     }
 
     @Override
     public LocalTransactionState checkLocalTransaction(MessageExt msg) {
-        Integer status = localTrans.get(msg.getTransactionId());
-        if (null != status) {
-            switch (status) {
-                case 0:
-                    return LocalTransactionState.UNKNOW;
-                case 1:
-                    return LocalTransactionState.COMMIT_MESSAGE;
-                case 2:
-                    return LocalTransactionState.ROLLBACK_MESSAGE;
-                default:
-                    return LocalTransactionState.COMMIT_MESSAGE;
-            }
-        }
+        System.out.println("执行到check,回查本地事务");
         return LocalTransactionState.COMMIT_MESSAGE;
     }
 }
--- example/src/main/java/org/apache/rocketmq/example/transaction/TransactionProducer.java
+++ example/src/main/java/org/apache/rocketmq/example/transaction/TransactionProducer.java
@@ -48,12 +48,11 @@ public class TransactionProducer {
         producer.setTransactionListener(transactionListener);
         producer.start();
 
-        String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             try {
                 Message msg =
-                    new Message("TopicTest1234", tags[i % tags.length], "KEY" + i,
-                        ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                        new Message("TopicTest6", "TagA", "KEY" + i,
+                                ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
                 SendResult sendResult = producer.sendMessageInTransaction(msg, null);
                 System.out.printf("%s%n", sendResult);
 
